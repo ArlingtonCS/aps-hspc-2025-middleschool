d002b57342b313a271130bf7f858d72ce71bed18
Nate Levin <63370104+NateLevin1@users.noreply.github.com>
2025-01-23 19:12:13 -0500

    Update sample.py

--- problems/F-soccer-passes/sample/sample.py
+++ problems/F-soccer-passes/sample/sample.py
@@ -1,12 +1,11 @@
 import math
 
+# Return a decimal number. `x1`, `y1`, `x2`, and `y2` are all decimal numbers.
 def calculate_distance(x1, y1, x2, y2):
     # WRITE CODE HERE
     
     return None # Modify this line
-
-
-
+    
 
 # DO NOT MODIFY BELOW HERE
 # Parse the input
