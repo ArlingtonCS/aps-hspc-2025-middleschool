f59fe2e23222ecc052618bc9d4b94ca04a89aa01
Nate Levin <63370104+NateLevin1@users.noreply.github.com>
2025-01-23 19:04:29 -0500

    Update sample.py

--- problems/D-photo-cleanup/sample/sample.py
+++ problems/D-photo-cleanup/sample/sample.py
@@ -5,11 +5,6 @@ def minimum_photos_to_delete(number_of_photos, total_memory, sizes):
     return None
 
 
-
-
-
-
-
 # DO NOT MODIFY ANYTHING BELOW HERE
 number_of_photos, total_memory = map(int, input().split())
 sizes = list(map(int, input().split()))
